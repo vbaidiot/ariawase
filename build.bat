109e28c7d72a68cd34e50c8870786508e99a7374
igeta <igeta.net@gmail.com>
2013-10-27 22:12:13 +0900

    change build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,7 @@
+@echo off
 cd %~dp0
+
+echo The vbac supports Word, Excel and Access Files. Which file do you want? 
+set /p ext=File Extension (default xlsm): 
+if "%ext%" neq "" (ren "src\Ariawase.xlsm" "Ariawase.%ext%")
 cscript //nologo vbac.wsf combine
