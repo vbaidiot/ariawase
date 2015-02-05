def0577eee8f63de681f6a59a16e91cee3e70dc1
dck-jp <dicek.am@clockahead.com>
2015-02-06 01:50:47 +0900

    Add c2sijs (C# program)
    rewrite build.bat to converts the encodings of source files to S-JIS
    before combine by c2sijs (C# program)

---
+++ c2sjis/c2sjis/Program.cs
@@ -0,0 +1,39 @@
+﻿using G_PROJECT;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c2sjis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sourceDirectory = Environment.CurrentDirectory + @"\" + (args.Length != 0 ?  args[0] : @"src");
+            if (!Directory.Exists(sourceDirectory)) return;
+          
+            var allFilePaths = Directory.GetFiles(sourceDirectory, "*", SearchOption.AllDirectories);
+            var targetFileExtensions = new List<string>() { "bas", "cls" };
+            allFilePaths
+                .Where(path => targetFileExtensions.Contains(Path.GetExtension(path)))
+                .Where(path => GetEncoding(path) != Encoding.GetEncoding("Shift-JIS"))
+                .ToList().ForEach(path => Convert2SJIS(path));
+        }
+
+        private static void Convert2SJIS(string path)
+        {
+            var encoding = new TxtEnc().SetFromTextFile(path);
+            var allText = File.ReadAllText(path, encoding);
+            File.WriteAllText(path, allText, encoding);
+        }
+
+        private static Encoding GetEncoding(string path)
+        {
+            var te = new TxtEnc();
+            return te.SetFromTextFile(path);
+        }
+    }
+}
